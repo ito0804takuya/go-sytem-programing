17a700332e61087e1a979fdff7c67375f47e81ae
ito0804takuya <ito0804takuya@gmail.com>
2022-06-01 22:44:09 +0900

    インターフェース利用例追加

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("Hello World!")
+// 	// fmt.Println("Hello", "World!") // 出力は同じ
+// }
+
+// ----------------------------------------------------
+type Talker interface {
+	Talk()
+}
+
+type Greeter struct {
+	name string
+}
+
+func (g Greeter) Talk() {
+	fmt.Printf("My name is %s\n", g.name)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	// greeter := 
+	var talker Talker
+	talker = Greeter{name: "jiro"}
+  talker.Talk()
 }
